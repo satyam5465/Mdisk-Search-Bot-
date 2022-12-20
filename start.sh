628955077058fceeb8c948608c4ecefd4efcb994
satyam5465 <104612279+satyam5465@users.noreply.github.com>
2022-12-20 16:23:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/satyam5465/Mdisk-Search-Bot-
+  git clone https://github.com/satyam5465/Mdisk-Search-Bot-.git /Mdisk-Search-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Mdisk-Search-Bot-
